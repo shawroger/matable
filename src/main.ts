353784f9025cc2cd89d82667457bfabeec23e3aa
shawroger <roger@shawroger.com>
2020-06-24 09:50:21 +0800

    correct type

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import "./plugin/muse-ui";
 import "./plugin/directives";
 import "./plugin/composition-api";
 
-import store from "./store";
+import store, { IStore } from "./store";
 import router from "./plugin/router";
 import { Config, mergeOptions } from "./utils/type";
 
@@ -16,7 +16,7 @@ class Matable {
 	VERSION = "1.2.6";
 	options: Config[] = [];
 
-	constructor(public globalConfig?: { title?: string; countdown?: string[] }) {}
+	constructor(public globalConfig?: IStore["globalConfig"]) {}
 
 	/**
 	 * @param config 用户的配置
@@ -38,10 +38,18 @@ class Matable {
 		 * 在 vuex 中混入配置数据
 		 */
 		store.state.config = this.options;
-		store.state.globalConfig = {
-			...store.state.globalConfig,
-			...this.globalConfig,
-		};
+
+		if (
+			typeof this.globalConfig !== "undefined" &&
+			typeof this.globalConfig.title !== "undefined" &&
+			typeof this.globalConfig.countdown !== "undefined"
+		) {
+			store.state.globalConfig = {
+				...store.state.globalConfig,
+				...this.globalConfig,
+			};
+		}
+
 		new this.vue({
 			store,
 			router,
@@ -52,7 +60,7 @@ class Matable {
 	}
 }
 
-export const init = (globalConfig?: { title?: string; countdown?: string[] }) =>
+export const init = (globalConfig?: IStore["globalConfig"]) =>
 	new Matable(globalConfig);
 
 if (process.env.NODE_ENV === "development") {
